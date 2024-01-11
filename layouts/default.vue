bf8afe619639dfc2cc76b70279fb8e722096e200
一纸忘忧 <i@ikxin.com>
2024-01-11 23:53:38 +0800

    feat: create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="h-50vh flex justify-center items-center">
+    <div>header</div>
+  </header>
+  <main><slot /></main>
+  <footer class="h-50vh flex justify-center items-center">
+    <div>footer</div>
+  </footer>
+</template>
