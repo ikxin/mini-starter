a26fdd49cc16f79945e72bdbb8f9b5f86216e557
一纸忘忧 <i@ikxin.com>
2024-01-14 23:56:39 +0800

    style: basic layout style of header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,20 @@
 <template>
-  <header class="h-[50vh] flex justify-center items-center">
-    <div>header</div>
+  <header>
+    <div class="h-16 px-4 max-w-6xl flex mx-auto justify-between items-center">
+      <!-- logo -->
+      <div class="flex-1">
+        <div class="w-32 h-8 bg-slate-200 rounded" />
+      </div>
+      <!-- menu -->
+      <ul class="flex items-center gap-2">
+        <li>Activate</li>
+        <li>Tools</li>
+        <li>Monitor</li>
+      </ul>
+      <!-- button -->
+      <div class="flex-1 flex items-center justify-end">
+        <UButton icon="i-heroicons-pencil-square" size="sm" square variant="ghost" />
+      </div>
+    </div>
   </header>
-  <main><slot /></main>
-  <footer class="h-[50vh] flex justify-center items-center">
-    <div>footer</div>
-  </footer>
 </template>
