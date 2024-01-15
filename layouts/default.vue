b88a3289deeb3ccaaa3e7e33dbdeef214281e0e5
一纸忘忧 <i@ikxin.com>
2024-01-15 17:22:46 +0800

    feat: switch to dark mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,39 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+</script>
+
 <template>
-  <header>
+  <header class="shadow-md">
     <div class="h-16 px-4 max-w-6xl flex mx-auto justify-between items-center">
-      <!-- logo -->
       <div class="flex-1">
         <div class="w-32 h-8 bg-slate-200 rounded" />
       </div>
-      <!-- menu -->
       <ul class="flex items-center gap-2">
         <li>Activate</li>
         <li>Tools</li>
         <li>Monitor</li>
       </ul>
-      <!-- button -->
       <div class="flex-1 flex items-center justify-end">
-        <UButton icon="i-heroicons-pencil-square" size="sm" square variant="ghost" />
+        <ClientOnly>
+          <UButton
+            :icon="isDark ? 'i-heroicons-moon-solid' : 'i-heroicons-sun-solid'"
+            color="gray"
+            variant="ghost"
+            @click="isDark = !isDark"
+          />
+          <template #fallback>
+            <div class="w-8 h-8" />
+          </template>
+        </ClientOnly>
       </div>
     </div>
   </header>
