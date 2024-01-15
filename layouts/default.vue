afb77d2a2e2679820a741002dc0b362f0ea7f3d3
一纸忘忧 <i@ikxin.com>
2024-01-15 22:29:54 +0800

    chore: prettier-plugin-tailwindcss

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,17 +16,17 @@ const currentYear = ref(dayjs().year())
 </script>
 
 <template>
-  <header class="sticky top-0 shadow-md border-b border-gray-200 dark:border-gray-800">
-    <div class="h-16 px-4 max-w-6xl flex mx-auto justify-between items-center">
+  <header class="sticky top-0 border-b border-gray-200 shadow-md dark:border-gray-800">
+    <div class="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
       <div class="flex-1">
-        <div class="w-32 h-8 bg-slate-200 rounded" />
+        <div class="h-8 w-32 rounded bg-slate-200" />
       </div>
       <ul class="flex items-center gap-2">
         <li>Activate</li>
         <li>Tools</li>
         <li>Monitor</li>
       </ul>
-      <div class="flex-1 flex items-center justify-end">
+      <div class="flex flex-1 items-center justify-end">
         <ClientOnly>
           <UButton
             :icon="isDark ? 'i-heroicons-moon-solid' : 'i-heroicons-sun-solid'"
@@ -35,7 +35,7 @@ const currentYear = ref(dayjs().year())
             @click="isDark = !isDark"
           />
           <template #fallback>
-            <div class="w-8 h-8" />
+            <div class="h-8 w-8" />
           </template>
         </ClientOnly>
       </div>
@@ -44,8 +44,8 @@ const currentYear = ref(dayjs().year())
   <section class="min-h-[calc(100vh-4rem)]">
     <slot />
   </section>
-  <footer class="text-gray-500 dark:text-gray-400 text-sm font-light">
-    <div class="h-14 mx-auto flex justify-center items-center gap-2 border-t border-gray-200 dark:border-gray-800">
+  <footer class="text-sm font-light text-gray-500 dark:text-gray-400">
+    <div class="mx-auto flex h-14 items-center justify-center gap-2 border-t border-gray-200 dark:border-gray-800">
       <div>
         <span>Copyright &copy; {{ currentYear }}</span>
         <UButton to="https://github.com/ikxin/quick-hub" target="_blank" variant="link" square class="font-light">
