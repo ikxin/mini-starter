a280a3fe8250cb7604042fc2ccaba1145a8ec7ce
一纸忘忧 <i@ikxin.com>
2024-01-15 22:06:38 +0800

    style: basic layout style of footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import dayjs from 'dayjs'
+
 const colorMode = useColorMode()
 
 const isDark = computed({
@@ -9,10 +11,12 @@ const isDark = computed({
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
   }
 })
+
+const currentYear = ref(dayjs().year())
 </script>
 
 <template>
-  <header class="shadow-md">
+  <header class="sticky top-0 shadow-md border-b border-gray-200 dark:border-gray-800">
     <div class="h-16 px-4 max-w-6xl flex mx-auto justify-between items-center">
       <div class="flex-1">
         <div class="w-32 h-8 bg-slate-200 rounded" />
@@ -37,4 +41,21 @@ const isDark = computed({
       </div>
     </div>
   </header>
+  <section class="min-h-[calc(100vh-4rem)]">
+    <slot />
+  </section>
+  <footer class="text-gray-500 dark:text-gray-400 text-sm font-light">
+    <div class="h-14 mx-auto flex justify-center items-center gap-2 border-t border-gray-200 dark:border-gray-800">
+      <div>
+        <span>Copyright &copy; {{ currentYear }}</span>
+        <UButton to="https://github.com/ikxin/quick-hub" target="_blank" variant="link" square class="font-light">
+          QuickHub
+        </UButton>
+      </div>
+      <div>
+        <span>Code with by</span>
+        <UButton to="https://www.ikxin.com" target="_blank" variant="link" square class="font-light"> ikxin </UButton>
+      </div>
+    </div>
+  </footer>
 </template>
